b30e584f244dc472d40ea1db1aa4566f9005ad43
Ryan Tate <ryan.tate@nike.com>
2020-01-09 13:15:56 -0800

    write jest config in package.json

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    roots: ['tests'],
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    collectCoverageFrom: ['src/**/*.ts'],
-    coveragePathIgnorePatterns: ['src/*.d.ts']
-};
